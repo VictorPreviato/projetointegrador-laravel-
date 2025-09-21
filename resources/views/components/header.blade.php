ed75b2ae86ec8ce41acb7c7a35c3ef0f5782a545
unknown <victorpreviato29@gmail.com>
2025-09-21 14:42:16 -0300

    comment botao ajuda navbar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -52,7 +52,7 @@
     <ul>
       <li><a href="{{ route('perfil') }}"><i class="ph-bold ph-user"></i>&nbsp;Meu perfil</a></li>
       <li><a href="{{ route('config-perfil') }}"><i class="ph-bold ph-gear-six"></i>&nbsp;Configurações</a></li>
-      <li><a href="#"><i class="ph-bold ph-question"></i>&nbsp;Ajuda</a></li>
+      <!-- <li><a href="#"><i class="ph-bold ph-question"></i>&nbsp;Ajuda</a></li> -->
       <form method="GET" action="{{ route('logout') }}">
     @csrf
       <li><a href="{{ route('logout') }}"><i class="ph-bold ph-sign-out"></i>&nbsp;Sair</a></li>
@@ -99,7 +99,7 @@
 
       <li><a class="a-perfil-mob" href="{{ route('perfil') }}"><i class="ph-bold ph-user"></i>&nbsp;Meu perfil</a></li>
       <li><a class="a-perfil-mob" href="{{ route('config-perfil') }}"><i class="ph-bold ph-gear-six"></i>&nbsp;Configurações</a></li>
-      <li><a class="a-perfil-mob" href="#"><i class="ph-bold ph-question"></i>&nbsp;Ajuda</a></li>
+      <!-- <li><a class="a-perfil-mob" href="#"><i class="ph-bold ph-question"></i>&nbsp;Ajuda</a></li> -->
       <form method="POST" action="{{ route('logout') }}">
     @csrf
       <li><a class="a-perfil-mob" href="{{ route('logout') }}"><i class="ph-bold ph-sign-out"></i>&nbsp;Sair</a></li>
